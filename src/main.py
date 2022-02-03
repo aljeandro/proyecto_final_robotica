c34fdb476736cb99246097fdfe601d920be8ccec
Alejandro Ortiz <alortizme@unal.edu.co>
2022-02-03 18:35:14 -0500

    ThingSpeak

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cv2
 from tennis_ball_detection import detect_ball, capture_image, graph_circle
 import robot_controller
+import requests
 
 #  ----------------------------------------------Constantes-------------------------------------------------------------
 # Parámetros de detección de circulos
@@ -13,8 +14,9 @@ MIN_RADIUS = 0
 MAX_RADIUS = 150
 
 # Constantes de conexión
-URL_CAM = "http://192.168.0.100:8080/shot.jpg"
+URL_CAM = "http://172.32.15.241:8080/shot.jpg"
 BLUETOOTH_PORT = 'COM5'
+THINGSPEAK_URL = "https://api.thingspeak.com/update?api_key=WPWVIJLCGRQDBWX4"
 
 # Constantes de resolución de la cámara
 CAPTURE_WIDTH = 666
@@ -30,6 +32,7 @@ LEFT_TURN_THRESHOLD = (1/3) * CAPTURE_WIDTH
 # ----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
+
     car = robot_controller.Car(BLUETOOTH_PORT)
 
     while True:
@@ -41,6 +44,9 @@ if __name__ == '__main__':
                                                                    PARAM1, PARAM2, MIN_RADIUS,
                                                                    MAX_RADIUS)
 
+        # action = 0 that the car is stopped.
+        action = 0
+
         if ball_is_detected:
             capture = graph_circle(capture, x_center, y_center, radius)
 
@@ -49,18 +55,25 @@ if __name__ == '__main__':
                 print("detencion")
             else:
                 if x_center >= RIGHT_TURN_THRESHOLD:
+                    action = 10
                     car.right_turn()
                     print('Giro a la derecha')
                 elif x_center <= LEFT_TURN_THRESHOLD:
+                    action = 20
                     car.left_turn()
                     print('Giro a la izquierda')
                 else:
+                    action = 30
                     car.straight_move()
                     print('Movimiento recto')
         else:
+
             car.stop()
             print("En espera")
 
+        requests.get(THINGSPEAK_URL, params={
+            "field1": action
+        })
         cv2.imshow(WINDOW_NAME, capture)
 
         # Press Esc key to exit
